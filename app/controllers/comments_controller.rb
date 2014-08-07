7592c14fb25cea87c1c256ff9b26256b86b3d758
tausema <ausemat@si.edu>
2014-08-07 16:28:12 -0400

    updated for comment field

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,17 +25,17 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
+    @transcription = @comment.transcription
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+   respond_to do |format|
+    if @comment.save
+      format.html { redirect_to transcription_path(@transcription), notice: 'Comment was successfully created.' }
+    else
+      format.html { render :new }
+      format.json { render json: @comment.errors, status: :unprocessable_entity }
     end
   end
+end 
 
   # PATCH/PUT /comments/1
   # PATCH/PUT /comments/1.json
